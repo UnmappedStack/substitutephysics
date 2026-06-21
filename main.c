7e6783be55434f858253f73bbfa82f03bb65ae21
UnmappedStack <jake.steinburger0@gmail.com>
2026-06-21 22:05:57 +1000

    make speedometre graphical

--- main.c
+++ main.c
@@ -7,17 +7,25 @@
 #define WIDTH  800
 #define HEIGHT 450
 
+#define SPEED_MAX 15
+#define SPEED_MIN  0
+
 typedef struct {
     float from, to;
 } Range;
 
+#define VECTOR(x, y) ((Vector2){x, y})
+#define MAP(value, old_min, old_max, new_min, new_max) (((value - old_min) / (old_max - old_min)) * (new_max - new_min) + new_min)
+
 int main(void) {
     struct pollfd pfd[1];
     InitWindow(WIDTH, HEIGHT, "Physics substitute");
+
+    Texture2D speedometre = LoadTexture("speedometre.png");
+
     float angle = 0;
     float velocity = 1;
     Range speed_range = { .from=5, .to=8 };
-    const char *goal_text = NULL;
     const char *right_text = NULL;
     struct Color goal_colour = RED;
     float time_out_of_bounds = 0.0;
@@ -29,8 +37,8 @@ int main(void) {
     const char *message = NULL;
 
     while (!WindowShouldClose()) {
-        if (IsKeyDown(KEY_W)) velocity += 0.05;
-        else if (IsKeyDown(KEY_S)) velocity -= 0.05;
+        if (IsKeyDown(KEY_W) && velocity < SPEED_MAX) velocity += 0.05;
+        else if (IsKeyDown(KEY_S) && velocity >= SPEED_MIN) velocity -= 0.05;
         else if (velocity > 0.01) velocity -= 0.025;
         BeginDrawing();
         ClearBackground(BLACK);
@@ -40,10 +48,17 @@ int main(void) {
             EndDrawing();
             continue;
         }
+        int speedometre_y = HEIGHT/2-speedometre.height/2;
+        int speedometre_x = WIDTH-speedometre.width-10;
+        DrawTexture(speedometre, speedometre_x, speedometre_y, RAYWHITE);
+        // idk how the following line works exactly i just kept changing the mapping until it seemed about right
+        int speedometre_level = MAP(velocity, 0, (speed_range.from+ speed_range.to)/2+speed_range.to, speedometre_y, speedometre_y + speedometre.height);
+        DrawTriangle(VECTOR(speedometre_x, speedometre_level+5),
+                     VECTOR(speedometre_x, speedometre_level+15),
+                     VECTOR(speedometre_x+10, speedometre_level+10),
+                     RAYWHITE); 
         DrawText("be careful, this game is pretty serious", 0, 0, 20, LIGHTGRAY);
-        DrawText(TextFormat("speed: %.1f", velocity), 0, 25, 20, LIGHTGRAY);
         if (velocity >= speed_range.from && velocity <= speed_range.to) {
-            goal_text = TextFormat("goal: %.1f-%.1f (within goal)", speed_range.from, speed_range.to);
             right_text = TextFormat("%.1f seconds to next level", secs_to_next_level);
             DrawText(right_text, WIDTH-MeasureText(right_text, 20), 0, 20, GREEN);
             goal_colour = GREEN;
@@ -60,16 +75,12 @@ int main(void) {
             }
         } else {
             bool above = velocity > speed_range.to;
-            goal_text = TextFormat("goal: %.1f-%.1f (outside goal!! %s)",
-                    speed_range.from, speed_range.to, (above) ? "slow down" : "speed up");
-            goal_colour = RED;
             time_out_of_bounds += 0.01;
             right_text = TextFormat("out of bounds for %.1f seconds\n(2 max)", time_out_of_bounds);
             if (time_out_of_bounds >= 2) game_running = false;
             DrawText(right_text, WIDTH-MeasureText(right_text, 20), 0, 20, RED);
             secs_to_next_level = 5;
         }
-        DrawText(goal_text, 0, 50, 20, goal_colour);
         DrawCircleSector((Vector2) {400, 225}, 100, angle, angle+45, 1, RAYWHITE); // TODO: make this a picture
         EndDrawing();
         angle += velocity;
